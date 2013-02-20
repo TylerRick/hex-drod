ecac9d4f633c969838f1bf7d141fc5eb292fe405
Hammerite <hammerite@orderofthehammer.com>
2013-02-20 08:41:25 +0000

    Functions to decide whether movement is possible on force arrows and step blockers

--- mathematics-functions.cpp
+++ mathematics-functions.cpp
@@ -1,5 +1,38 @@
 #include "mathematics-functions.h"
 
 int iRoundAwayFromZero (float f) {
+    assert(f > -1e16);
+    assert(f <  1e16);
     return (int)(f > 0 ? floor(f + 0.5) : ceil(f - 0.5));
+}
+
+bool forceArrowPermitsMovement (const int forceArrowDirection, const int movementDirection) {
+    static const bool movementPermitted[7][6] = {
+        {true , true , false, false, false, true }, // Force arrow pointing north
+        {true , true , true , false, false, false}, // Force arrow pointing northeast
+        {false, true , true , true , false, false}, // Force arrow pointing southeast
+        {false, false, true , true , true , false}, // Force arrow pointing south
+        {false, false, false, true , true , true }, // Force arrow pointing southwest
+        {true , false, false, false, true , true }, // Force arrow pointing northwest
+        {true , true , true , true , true , true }  // No force arrow present on hex
+    };
+    assert(forceArrowDirection >= 0);
+    assert(forceArrowDirection <  7);
+    assert(movementDirection   >= 0);
+    assert(movementDirection   <  6);
+    return movementPermitted[forceArrowDirection][movementDirection];
+}
+
+bool stepBlockerPermitsMovement (const int stepBlockerTypes, const int movementDirection) {
+    static const bool movementPermitted[4][6] = {
+        {true , true , true , true , true , true }, // No step blocker present on hex
+        {false, true , false, true , false, true }, // Step blocker preventing positive-axis movement
+        {true , false, true , false, true , false}, // Step blocker preventing negative-axis movement
+        {false, false, false, false, false, false}  // Both types of step blockers are present
+    };
+    assert(stepBlockerTypes  >= 0);
+    assert(stepBlockerTypes  <  4);
+    assert(movementDirection >= 0);
+    assert(movementDirection <  6);
+    return movementPermitted[stepBlockerTypes][movementDirection];
 }
--- mathematics-functions.h
+++ mathematics-functions.h
@@ -1,8 +1,22 @@
 #ifndef MATHEMATICS_FUNCTIONS_H
     #define MATHEMATICS_FUNCTIONS_H
 
+    #include <assert.h>
     #include <math.h>
 
     int iRoundAwayFromZero (float f);
+        // This function asserts that its argument is less than 10^16 in
+        // absolute value. This is because the method it uses to perform
+        // rounding may give inaccurate results for floats that are very large
+        // in absolute value.
 
+    bool forceArrowPermitsMovement (const int forceArrowDirection, const int movementDirection);
+    bool stepBlockerPermitsMovement (const int stepBlockerTypes, const int movementDirection);
+        // These functions perform unchecked array dereferencing (against
+        // appropriate static const arrays) using the supplied integer
+        // arguments. forceArrowDirection should be in the range 0 to 6
+        // inclusive; stepBlockerTypes should be in the range 0 to 3 inclusive;
+        // and in each case movementDirection should be in the range 0 to 5
+        // inclusive. Each function asserts that these requirements are
+        // satisfied.
 #endif
