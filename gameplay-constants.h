b8fba14b4dbe4967909e0ec2fb35627d0e8ec474
Hammerite <hammerite@orderofthehammer.com>
2013-03-01 23:17:45 +0000

    Add token-type constants file

--- gameplay-constants.h
+++ gameplay-constants.h
@@ -9,6 +9,12 @@
     #define DIRECTION_SOUTH     3
     #define DIRECTION_SOUTHWEST 4
     #define DIRECTION_NORTHWEST 5
+    #define FORCE_ARROW_NONE    6
+
+    #define STEP_BLOCKER_NONE          0
+    #define STEP_BLOCKER_POSITIVE_AXIS 1
+    #define STEP_BLOCKER_NEGATIVE_AXIS 2
+    #define STEP_BLOCKER_ALL           3
 
     #define BOMB_BLAST_RADIUS 4
 #endif
---
+++ token-types.h
@@ -0,0 +1,24 @@
+#ifndef TOKEN_TYPES_H
+    #define TOKEN_TYPES_H
+
+    #define TOKEN_NOTHING                      0
+    #define TOKEN_AUTOSAVE                     1
+    #define TOKEN_PERSISTENT_CITIZEN_MOVEMENT  2
+    #define TOKEN_ROOM_CLEAR                   3
+    #define TOKEN_SHRIEK                       4
+    #define TOKEN_ACTIVE                       5
+    #define TOKEN_PASSIVE                      6
+    #define TOKEN_TOGGLE_SWORD                 7
+    #define TOKEN_TOGGLE_GRAVITY               8
+    #define TOKEN_FORCE_ARROWS_REVERSE         9
+    #define TOKEN_FORCE_ARROWS_CLOCKWISE      10
+    #define TOKEN_FORCE_ARROWS_ANTICLOCKWISE  11
+    #define TOKEN_REVERSE_SNAKES              12
+    #define TOKEN_SWITCH_RATTLESNAKE_ADDER    13
+    #define TOKEN_SWITCH_RATTLESNAKE_SERPENT  14
+    #define TOKEN_SWITCH_ADDER_SERPENT        15
+    #define TOKEN_SWITCH_GOBLIN_GOLEM         16
+    #define TOKEN_SWITCH_MUD_TAR              17
+    #define TOKEN_SWITCH_MUD_GEL              18
+    #define TOKEN_SWITCH_TAR_GEL              19
+#endif
