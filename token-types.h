cb6b71500fd265d75d4c6ea5c40b35e69eee10b4
Hammerite <hammerite@orderofthehammer.com>
2013-03-02 20:25:26 +0000

    Entity layer type constants

--- token-types.h
+++ token-types.h
@@ -1,24 +1,27 @@
 #ifndef TOKEN_TYPES_H
     #define TOKEN_TYPES_H
 
-    #define TOKEN_NOTHING                      0
-    #define TOKEN_AUTOSAVE                     1
-    #define TOKEN_PERSISTENT_CITIZEN_MOVEMENT  2
-    #define TOKEN_ROOM_CLEAR                   3
-    #define TOKEN_SHRIEK                       4
-    #define TOKEN_ACTIVE                       5
-    #define TOKEN_PASSIVE                      6
-    #define TOKEN_TOGGLE_SWORD                 7
-    #define TOKEN_TOGGLE_GRAVITY               8
-    #define TOKEN_FORCE_ARROWS_REVERSE         9
-    #define TOKEN_FORCE_ARROWS_CLOCKWISE      10
-    #define TOKEN_FORCE_ARROWS_ANTICLOCKWISE  11
-    #define TOKEN_REVERSE_SNAKES              12
-    #define TOKEN_SWITCH_RATTLESNAKE_ADDER    13
-    #define TOKEN_SWITCH_RATTLESNAKE_SERPENT  14
-    #define TOKEN_SWITCH_ADDER_SERPENT        15
-    #define TOKEN_SWITCH_GOBLIN_GOLEM         16
-    #define TOKEN_SWITCH_MUD_TAR              17
-    #define TOKEN_SWITCH_MUD_GEL              18
-    #define TOKEN_SWITCH_TAR_GEL              19
+    #define TOKEN_NOTHING                         0
+    #define TOKEN_AUTOSAVE                        1
+    #define TOKEN_PERSISTENT_CITIZEN_MOVEMENT     2
+    #define TOKEN_ROOM_CLEAR                      3
+    #define TOKEN_SHRIEK                          4
+    #define TOKEN_ACTIVE                          5
+    #define TOKEN_PASSIVE                         6
+    #define TOKEN_TOGGLE_SWORD                    7
+    #define TOKEN_TOGGLE_GRAVITY                  8
+    #define TOKEN_FORCE_ARROWS_REVERSE            9
+    #define TOKEN_FORCE_ARROWS_CLOCKWISE         10
+    #define TOKEN_FORCE_ARROWS_ANTICLOCKWISE     11
+    #define TOKEN_REVERSE_SERPENTS               12
+    #define TOKEN_SWITCH_RATTLESNAKE_ADDER       13
+    #define TOKEN_SWITCH_RATTLESNAKE_WATER_SNAKE 14
+    #define TOKEN_SWITCH_RATTLESNAKE_COBRA       15
+    #define TOKEN_SWITCH_ADDER_WATER_SNAKE       16
+    #define TOKEN_SWITCH_ADDER_COBRA             17
+    #define TOKEN_SWITCH_WATER_SNAKE_COBRA       18
+    #define TOKEN_SWITCH_MUD_TAR                 19
+    #define TOKEN_SWITCH_MUD_GEL                 20
+    #define TOKEN_SWITCH_TAR_GEL                 21
+    #define TOKEN_SWITCH_GOBLIN_GOLEM            22
 #endif
